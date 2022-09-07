a43ed3e25c60f08d1c32c8432c960255c479c351
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2022-09-07 20:14:23 -0300

    Update Controller.java

--- src/main/java/persistencia/Controller.java
+++ src/main/java/persistencia/Controller.java
@@ -12,7 +12,56 @@ public class Controller implements IController {
 	private HashMap<String, Institucion> ins;
 	
 	public Controller() {
+		ins=null;
+		act=null;
+		cpn=null;
+		usr=null;
 		
+	}
+	
+	public HashMap<String, Usuario> getUsr(){
+		return usr;
+	}
+	
+	public HashMap<String, Cuponera> getCpn(){
+		return cpn;
+	}
+	
+	public HashMap<String, Actividad> getAct(){
+		return act;
+	}
+	
+	public HashMap<String, Institucion> getIns(){
+		return ins;
+	}
+	
+	public void altaInstitucion(String nombre, String desc, String url) {
+		if(!ins.containsKey(nombre)) {
+			Institucion d=new Institucion(nombre,desc,url);
+			ins.put(nombre,d);
+		}else {/*tira excepcion*/}
+	}
+	
+	public void altaSocio(String nick, String nom, String ap, String mail, Fecha fnac, String foto) {
+		if(!usr.containsKey(nick)) {
+			Usuario u=new Socio(nick,nom,ap,mail,fnac,foto);
+			usr.put(nick,u);
+		}else {/*tira excepcion*/}
+	}
+	
+	public void altaProf(String nick, String nom, String ap, String mail, String desc, String bio, String web, Fecha fnac, Institucion i, String foto) {
+		if(!usr.containsKey(nick)) {
+			Usuario u=new Profesor(nick,nom,ap,mail,desc,bio,web,fnac,i,foto);
+			usr.put(nick,u);
+		}else {/*tira excepcion*/}
+	}
+	
+	public void altaActividad(String nom, String desc,  int duracion, int costo, Fecha fecha,Institucion i) {
+		if(!act.containsKey(nom)) {
+			Actividad a=new Actividad(nom,desc,duracion,costo,fecha,i);
+			act.put(nom, a);
+			i.getActividades().add(a);
+		}else {/*tira excepcion*/}
 	}
 
 }
