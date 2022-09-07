f8e49169b93680d8a5bb66eb9a8f2cee967e0a60
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2022-09-07 20:13:27 -0300

    Update IController.java

--- src/main/java/persistencia/IController.java
+++ src/main/java/persistencia/IController.java
@@ -2,5 +2,13 @@ package persistencia;
 
 
 public interface IController {
+	public abstract HashMap<String, Usuario> getUsr();
+	public abstract HashMap<String, Cuponera> getCpn();
+	public abstract HashMap<String, Actividad> getAct();
+	public abstract HashMap<String, Institucion> getIns();
+	public abstract void altaInstitucion(String nombre, String desc, String url);
+	public abstract void altaSocio(String nick, String nom, String ap, String mail, Fecha fnac, String foto);
+	public abstract void altaProf(String nick, String nom, String ap, String mail, String desc, String bio, String web, Fecha fnac, Institucion i, String foto);
+	public abstract void altaActividad(String nom, String desc,  int duracion, int costo, Fecha fecha, Institucion i);
 
 }
