4fc2931739265bb67d0b4ac34ea0578bdc38404d
Frankb-eng <55456165+Frankb-eng@users.noreply.github.com>
2022-09-21 10:13:25 -0300

    agrgado cabezal abstracto de funcion inicializar

--- src/main/java/persistencia/IController.java
+++ src/main/java/persistencia/IController.java
@@ -12,5 +12,6 @@ public interface IController {
 	public abstract void altaSocio(String nick, String nom, String ap, String mail, LocalDate fnac, String foto);
 	public abstract void altaProf(String nick, String nom, String ap, String mail, String desc, String bio, String web, LocalDate fnac, Institucion i, String foto);
 	public abstract void altaActividad(String nom, String desc,  int duracion, int costo, LocalDate fecha, Institucion i);
+	public abstract void inicializar() throws SQLException;
 
 }
